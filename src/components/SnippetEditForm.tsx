ba5d0363495177d411842665f858efd572d6c1a0
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2025-03-07 08:04:57 -0400

    SnippetEditPage - using Monaco editor.

--- src/components/SnippetEditForm.tsx
+++ src/components/SnippetEditForm.tsx
@@ -1,14 +1,36 @@
 'use client';
 
 import { Snippet } from '@prisma/client';
+import Editor, { OnChange } from '@monaco-editor/react';
+import { useCallback, useMemo, useState } from 'react';
+import { editor } from 'monaco-editor';
 
 type Props = Readonly<{
   snippet: Snippet
 }>
 
 export default function SnippetEditForm({ snippet }: Props) {
+  const [code, setCode] = useState<string | undefined>(snippet.code);
+
+  const handleEditorChange: OnChange = useCallback((value, ev) => {
+    console.log({value})
+    setCode(value);
+  }, []);
+
+  const options: editor.IStandaloneEditorConstructionOptions = useMemo(() => ({
+    minimap: { enabled: false }
+  }), []);
 
   return (
-    <div>Client Component has snippet with title {snippet.title}</div>
+    <div>
+      <Editor
+        height="40vh"
+        theme="vs-dark"
+        defaultLanguage="javascript"
+        options={options}
+        defaultValue={snippet.code}
+        onChange={handleEditorChange}
+      />
+    </div>
   );
 }
